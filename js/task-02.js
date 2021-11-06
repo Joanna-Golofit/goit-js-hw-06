07fe029c319ab0eafef1c732cad3e77312171a44
Joanna <81862821+Joanna-Golofit@users.noreply.github.com>
2021-11-06 13:32:08 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,19 +7,55 @@ const ingredients = [
   'Condiments',
 ];
 
-const list = document.querySelector("#ingredients");
 
-ingredients.map(ingredient => {
-  const listItem = document.createElement("li");
-  listItem.textContent = ingredient;
-  listItem.classList.add("item");
-  console.log(listItem);
-  console.log(listItem.innerHTML);
-  console.log(typeof listItem);
+const ingredientsList = document.querySelector("ul#ingredients");
 
-  // console.log([...listItem]);
-  // console.log(typeof [...listItem]);
-  list.append(listItem);
-  return listItem;
+const markup = ingredients.map(ingredient => {
+  const li = document.createElement("li");
+  li.setAttribute("class", "nature"); // najpierw setAttribute a potem classList dziala, bo inaczej nadpisuje klase
+  li.classList.add("item");
+  li.innerText = ingredient;
+  console.log(li);
+  ingredientsList.append(li);
+
+  return li; // zeby cos w markupie zostalo
 });
+//   console.log(...markup);
+//   console.log(markup.join(""));
+
+// ingredientsList.append(markup);
+// ingredientsList.insertAdjacentHTML("beforeend", ...markup);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =========druga wersja
+
+
+// const list = document.querySelector("#ingredients");
+
+// ingredients.map(ingredient => {
+//   const listItem = document.createElement("li");
+//   listItem.textContent = ingredient;
+//   listItem.classList.add("item");
+//   console.log(listItem);
+//   console.log(listItem.innerHTML);
+//   console.log(typeof listItem);
+
+//   // console.log([...listItem]);
+//   // console.log(typeof [...listItem]);
+//   list.append(listItem);
+//   return listItem;
+// });
 
