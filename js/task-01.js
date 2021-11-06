6c702e29a6addcf26231d040fd9f39f73712883a
Joanna <81862821+Joanna-Golofit@users.noreply.github.com>
2021-11-06 09:03:32 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -7,3 +7,25 @@ for (let i = 0; i < items.length; i++) {
   console.log("Category: ", headings[0].innerText);
   console.log("Elements: ", listItems.length);
 }
+
+// inne wersje tego samego:
+
+// //wersja 1
+// const categories = document.querySelector("ul#categories");
+// console.log("Number of categories: " + categories.childElementCount);
+
+// //wersja 2
+// const categories2 = document.querySelectorAll("li.item");
+// console.log("Number of categories2: " + categories2.length);
+
+// //wersja 3
+// console.log(
+//   "Number of categories3: " + document.querySelectorAll("li.item").length
+// );
+
+// categories2.forEach(c => {
+//   console.log("Category:", c.firstElementChild.innerHTML);
+//   console.log(`Elements: ${c.querySelectorAll("li").length}`);
+// })
+
+
