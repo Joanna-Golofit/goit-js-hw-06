1eb3f9686f36a4892ee9773dc09fccf969cf65e2
Joanna <81862821+Joanna-Golofit@users.noreply.github.com>
2021-11-06 21:40:07 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,44 @@
 const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
-nameInput.addEventListener("input", (event) => {
-  nameOutput.textContent = event.currentTarget.value;
-  // console.log(event.currentTarget.value);
-  if (event.currentTarget.value === "") {
-    nameOutput.textContent = "Anonymous";
-   }
-});
+const update = (e) => {
+  nameOutput.innerHTML = e.target.value;
+  if (!e.target.value) {
+        nameOutput.innerHTML = "Anonymous";
+  }
+}
+
+nameInput.addEventListener("input", update)
+
+
+
+
+
+
+
+// wersja 2:
+
+// const update = () => {
+//   nameOutput.innerHTML = nameInput.value;
+//   if (!nameInput.value) {
+//     nameOutput.innerHTML = "Anonymous";
+//   }
+// };
+
+// nameInput.addEventListener("input", update);
+
+
+
+
+//================== pierwsza wersja
+// const nameInput = document.querySelector("#name-input");
+// const nameOutput = document.querySelector("#name-output");
+
+// nameInput.addEventListener("input", (event) => {
+//   nameOutput.textContent = event.currentTarget.value;
+//   // console.log(event.currentTarget.value);
+//   if (event.currentTarget.value === "") {
+//     nameOutput.textContent = "Anonymous";
+//    }
+// });
 
