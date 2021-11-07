ae3c3e5521e126a47abf09784f8b3093a2f88fb8
Joanna <81862821+Joanna-Golofit@users.noreply.github.com>
2021-11-07 09:25:23 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -1,14 +1,50 @@
 const qs = (selector) => document.querySelector(selector);
-// const log = (message) => console.log(message);
 
-const range = qs("input#font-size-control");
+const range = qs("#font-size-control");
 const text = qs("span#text");
 
+const changeFontSize = (e) => {
+  console.log(`${e.target.value}px`);
+  text.style.fontSize = `${e.target.value}px`;
+};
+
+range.addEventListener("click", changeFontSize);
 
 
-range.addEventListener("click", () => {
-  text.style.fontSize = `${range.value}px`;
-});
+
+
+
+
+
+
+// =============== druga wersja
+
+// const range = document.querySelector("#font-size-control");
+// const text = document.querySelector("span#text");
+
+// range.addEventListener("click", (e) => {
+//   console.log(e.target.value);  
+//   text.style.fontSize = `${e.target.value}px`;
+// });
+
+
+
+
+
+
+
+// =============== pierwsza wersja
+// const qs = (selector) => document.querySelector(selector);
+// // const log = (message) => console.log(message);
+
+// const range = qs("input#font-size-control");
+// const text = qs("span#text");
+
+
+
+// range.addEventListener("click", () => {
+//   text.style.fontSize = `${range.value}px`;
+// });
 
 
 
